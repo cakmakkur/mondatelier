03060b5dfc33d165fb1d95425116e143f380d1b2
kursat <cakmakkur@hotmail.com>
2025-09-09 22:32:34 +0200

    9-9

--- frontend/src/components/userActions/CommunitySearchBar.tsx
+++ frontend/src/components/userActions/CommunitySearchBar.tsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import type { CommunityDto } from "../../dto/CommunityDto";
+import type { PostDto } from "../../dto/PostDto";
+
+const COMMUNITIES_PATH = import.meta.env.VITE_COMMUNITIES_PATH;
+
 export default function CommunitySearchBar() {
+  const [searchResult, setSearchResult] = useState<(CommunityDto | PostDto)[]>(
+    []
+  );
+
+  const search = async (query: string) => {
+    try {
+      const response = await fetch(
+        `${COMMUNITIES_PATH}/search?query=${query}&page=${searchPage}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setSearchResult(data);
+      }
+    } catch (error) {
+      console.error("Error fetching communities:", error);
+    }
+  };
   return (
     <div className="community-search-bar-main">
       <input
