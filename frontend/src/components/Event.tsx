6f7d081852757e8e516e8c44603deed863898d88
kursat <cakmakkur@hotmail.com>
2025-10-25 14:26:35 +0200

    commit

--- frontend/src/components/Event.tsx
+++ frontend/src/components/Event.tsx
@@ -48,13 +48,10 @@ export default function Event({ event }: EventPropTypes) {
           <span className="event_profile_name">{profile?.profileName}</span>
         </div>
       </section>
-      <menu
-        className="d-flex flex-row align-items-center w-100"
-        style={{ marginTop: "10px" }}
-      >
+      {/* <menu className="d-flex flex-row w-100" style={{ marginTop: "10px" }}>
         <img className="event_buttons__like" src="/heart.svg" alt="" />
         <img src="/share2.svg" alt="" />
-      </menu>
+      </menu> */}
     </div>
   );
 }
--- frontend/src/components/EventModalView.tsx
+++ frontend/src/components/EventModalView.tsx
@@ -3,6 +3,9 @@ import type { EventDto } from "../dto/EventDto";
 import type { RootState } from "../store/store";
 import { Link } from "react-router-dom";
 import { useModalContext } from "../context/ModalContext";
+import { useEffect, useRef, useState } from "react";
+import useAxiosPrivate from "../auth/useAxiosPrivate";
+import { useAuthContext } from "../auth/AuthContext";
 
 interface EventPropTypes {
   event: EventDto;
@@ -13,7 +16,56 @@ const UPLOADS_PATH = import.meta.env.VITE_MEDIA_URL;
 export default function EventModalView({ event }: EventPropTypes) {
   const profiles = useSelector((state: RootState) => state.event.profiles);
   const profile = profiles[event.profileId];
+  const heartIconEmptyRef = useRef<HTMLImageElement>(null);
+  const heartIconFillRef = useRef<HTMLImageElement>(null);
+  const axiosPrivate = useAxiosPrivate();
+  const { auth } = useAuthContext();
+  const [isLiked, setIsLiked] = useState(false);
+
   const { setComponentState } = useModalContext();
+
+  const handleLikeClick = async () => {
+    if (!auth) return; // here sign in modal
+    if (isLiked) {
+      setIsLiked(false);
+    } else {
+      setIsLiked(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!heartIconEmptyRef.current || !heartIconFillRef.current) return;
+
+    if (isLiked) {
+      // liked → show fill, hide empty
+      heartIconFillRef.current.classList.remove(
+        "post-profile--right-like-inactive"
+      );
+      heartIconFillRef.current.classList.add("post-profile--right-like-active");
+
+      heartIconEmptyRef.current.classList.remove(
+        "post-profile--right-like-active"
+      );
+      heartIconEmptyRef.current.classList.add(
+        "post-profile--right-like-inactive"
+      );
+    } else {
+      // not liked → show empty, hide fill
+      heartIconEmptyRef.current.classList.remove(
+        "post-profile--right-like-inactive"
+      );
+      heartIconEmptyRef.current.classList.add(
+        "post-profile--right-like-active"
+      );
+
+      heartIconFillRef.current.classList.remove(
+        "post-profile--right-like-active"
+      );
+      heartIconFillRef.current.classList.add(
+        "post-profile--right-like-inactive"
+      );
+    }
+  }, [isLiked]);
 
   return (
     <div className="event_popup_wrapper">
@@ -51,9 +103,23 @@ export default function EventModalView({ event }: EventPropTypes) {
             </Link>
           </div>
           <menu className="d-flex flex-row align-items-center w-100">
-            <button className="event_buttons__like">
-              <img src="/heart.svg" alt="event like button" />
-            </button>
+            <span
+              onClick={handleLikeClick}
+              className="post-profile--right-like"
+            >
+              <img
+                ref={heartIconEmptyRef}
+                className="post-profile--right-like-empty"
+                src="/heart.svg"
+                alt="like icon"
+              />
+              <img
+                ref={heartIconFillRef}
+                className="post-profile--right-like-fill"
+                src="/heart-fill.svg"
+                alt="unlike icon"
+              />
+            </span>
             <button className="event_buttons__like">
               <img src="/share.svg" alt="event share button" />
             </button>
